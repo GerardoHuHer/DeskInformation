5c1aa108e8b2fa38a289074caa61473efa36e9a0
Gerardo Huerta Hernández <gerardohuerta1502@gmail.com>
2023-12-29 19:00:40 -0600

    Se empezó con la interfaz y también con el diseño de la clase usuario para hacerlo multiusuario

---
+++ funciones.py
@@ -0,0 +1,7 @@
+class User:
+    def __init__(self, userName, password, directory):
+        self.__userName = userName
+        self.__password = password
+        self.__directory = directory
+
+    
--- interfaz.py
+++ interfaz.py
@@ -1,3 +1,23 @@
 import tkinter as tk
 import customtkinter as ctk
 
+class Interface(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.title("Information")
+        self.crear_interfaz()
+
+    def crear_interfaz(self):
+        self.github_token = ctk.CTkLabel(self, text = "GitHub Token: ")
+        self.github_token.grid(column = 0, row = 0)
+        self.task_button = ctk.CTkButton(self, text = "Add Task")
+        self.task_button.grid(column = 1, row = 1) 
+
+        self.quit_button = ctk.CTkButton(self, text="Quit", command = self.quitar)
+        self.quit_button.grid(column = 1, row = 2)
+    def quitar(self):
+        self.destroy()
+
+if __name__ == "__main__":
+    app = Interface()
+    app.mainloop()
